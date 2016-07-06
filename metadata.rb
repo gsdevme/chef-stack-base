8ae42f5f482e84c52863093b7fed1527befb98b0
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-07-06 22:50:18 +0100

    chore: decrease apt to 2.9

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ version          '0.0.1'
 
 depends 'yum-epel', '~> 0.6.5'
 depends 'yum-ius', '~> 0.4.5'
-depends 'apt', '~> 3.0.0'
+depends 'apt', '~> 2.9.2'
 depends 'cron', '~> 1.7.6'
 depends 'glances', '~> 1.0.3'
 depends 'fail2ban', '~> 2.3.0'
